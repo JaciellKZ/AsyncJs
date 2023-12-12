cb9247586f8092bf24304407f4d9716984537b90
kevin <kevinzambranocastro@gmail.com>
2023-12-12 14:10:45 -0400

    documenting what has been done

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,21 +1,23 @@
+//here we receive the api and contain it
 const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCTSbqSJJOUH782VpV1FWtlg&part=snippet%2Cid&order=date&maxResults=9';
-const content = null || document.getElementById('content');
-//468d9bd2bamsh8ccc799a5015bd9p1d5e92jsn3fab090cf730
+const content = null || document.getElementById('content');//here we call id: content and we put the data brought from our api in this content already created with tailwind
+
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': `${key}`,
 		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
 	}
-};
+};//the method to tell if we are giving or getting data from the api, and specify the key and the host of the api.
 
-async function fetchData(urlApi) 
+async function fetchData(urlApi) //we create the function fetchData
 {
     const response = await fetch(urlApi,options)
     const data = await response.json();
     return data;
 }
 
+//here is what we are going to see when everything works well or if there is any error, if everything goes well, it should show the structure that we did
 (async () => {
 
     try {
